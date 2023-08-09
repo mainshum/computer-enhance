c9796c1267cd479d179733f68307c19a882364a5
mainshum <doseit@gmail.com>
2023-08-09 11:36:54 +0200

    update: Entire 39 tackled

--- part1/test.asm
+++ part1/test.asm
@@ -40,3 +40,8 @@ mov ah, [bx + si + 4]
 
 ; Source address calculation plus 16-bit displacement
 mov al, [bx + si + 4999]
+
+; Dest address calculation
+mov [bx + di], cx
+mov [bp + si], cl
+mov [bp], ch
